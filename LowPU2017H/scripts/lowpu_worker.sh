c6e5e27fc28581b5f39cee9530e9f2bca324aa0e
Pedro <psilva@cern.ch>
2021-10-04 09:56:33 +0200

    updating scripts

--- LowPU2017H/scripts/lowpu_worker.sh
+++ LowPU2017H/scripts/lowpu_worker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-input=${1}
-output=${2}
-cmssw=${3}
+proxy=${1}
+input="root://cms-xrd-global.cern.ch/${2}"
+output=${3}
+cmssw=${4}
 
 cd ${cmssw}/src
 eval `scram r -sh`
-export X509_USER_PROXY=${cmssw}/src/PPSTools/LowPU2017H/data/voms_proxy.txt
+export X509_USER_PROXY=${proxy}
+voms-proxy-info -all
+voms-proxy-info -all -file ${proxy}
 cd -
 
 $CMSSW_BASE/src/PhysicsTools/NanoAODTools/scripts/nano_postproc.py output ${input} \
--- LowPU2017H/scripts/processDataset.py
+++ LowPU2017H/scripts/processDataset.py
@@ -9,16 +9,17 @@ def main():
     parser = optparse.OptionParser(usage)
     parser.add_option('-i', '--in',     dest='input',  help='input dataset',    default='/SingleMuon/Run2017H-UL2017_MiniAODv1_NanoAODv2-v1/NANOAOD', type='string')
     parser.add_option('-o', '--out',    dest='output', help='output directory', default='/eos/user/p/psilva/data/sdanalysis/SingleMuon/Chunks', type='string')
-    parser.add_option('--dry',          dest='dry',    help='don\'t submit', action='store_true')
+    parser.add_option('--submit',       dest='submit', help='submit jobs', action='store_true')
     (opt, args) = parser.parse_args()
 
     cmssw=os.environ['CMSSW_BASE']
 
-    #start a proxy
+    #warn user to start a proxy
     proxy='{}/src/PPSTools/LowPU2017H/data/voms_proxy.txt'.format(cmssw)
     print('Warning: assuming you have a valid proxy under {}'.format(proxy))
     print('If not the case, before submitting to condor, please start one with')
-    print('voms-proxy-init --voms cms --valid 172:00 --out {}'.format(proxy))
+    print('voms-proxy-init --voms cms')
+    print('cp `voms-proxy-info -p` {}'.format(proxy))
         
     #get list of files
     file_list=os.popen('dasgoclient --query=\"file dataset={} status=*\"'.format(opt.input)).read().split()
@@ -30,19 +31,20 @@ def main():
     condor_script='{}_condor.sub'.format( opt.input.replace('/','_')[1:] )
     scripts_dir='{}/src/PPSTools/LowPU2017H/scripts'.format(cmssw)
     with open (condor_script,'w') as condor:
+        condor.write('Proxy_path = {}\n'.format(proxy))
         condor.write('executable = {}/lowpu_worker.sh\n'.format(scripts_dir))
+        condor.write('arguments = $(Proxy_path) $(rfile) {} {}\n'.format(opt.output,cmssw))
         condor.write('output     = jobs$(ClusterId).out\n')
         condor.write('error      = jobs$(ClusterId).err\n')
         condor.write('log        = jobs$(ClusterId).log\n')
         condor.write('+JobFlavour = "testmatch"\n')
-        condor.write('arguments = $(input) {} {}\n'.format(opt.output,cmssw))
-        condor.write('queue input from (\n')
+        condor.write('queue rfile from (\n')
         for f in file_list:
-            condor.write('\troot://cms-xrd-global.cern.ch/{}\n'.format(f.strip()))
+            condor.write('\t{}\n'.format(f.strip()))
         condor.write(')\n')
 
     #submit to condor
-    if not opt.dry:
+    if opt.submit:
         os.system('condor_submit {}'.format(condor_script))
 
 
